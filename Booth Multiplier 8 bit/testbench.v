cfe307f10b4a4f5cb0770db751a3d035f1cb6581
LALIT ARORA <lalitarora1997@gmail.com>
2017-08-14 12:30:57 +0530

    Booth Multiplier Algorithm 8-bit

---
+++ Booth Multiplier 8 bit/testbench.v
@@ -0,0 +1,20 @@
+
+module testbench;
+reg clk, start;
+reg [7:0] a, b;
+wire [15:0] ab;
+wire busy;
+multiplier multiplier1(ab, busy, a, b, clk, start);
+initial begin
+clk = 0;
+$display("first example: a = 3 b = 17");
+a = 3; b = 17; start = 1; #50 start = 0;
+#80 $display("first example done");
+$display("second example: a = 7 b = 7");
+a = 7; b = 7; start = 1; #50 start = 0;
+#80 $display("second example done");
+$finish;
+end
+always #5 clk = !clk;
+always @(posedge clk) $strobe("ab: %d busy: %d at time=%t", ab, busy, $stime);
+endmodule
